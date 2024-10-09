3c1f091c5d81f69d6d00df353056509f58c5be28
RishikaBhatKuthyar <kuthyar.ri@northeastern.edu>
2024-10-09 15:14:05 -0400

    Added frontend using react

---
+++ Microservices/microservices-frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import EmployeeComponent from './component/employeeComponent';
+
+
+function App() {
+  return (
+    <div className="container">
+      <EmployeeComponent/>
+    </div>
+  );
+}
+
+export default App;
